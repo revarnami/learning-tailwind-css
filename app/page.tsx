2775628b36b7ba265fe3155c7cc61d4fa5bc56c2
Fauzi Arnami <fauzi.arnami@gmail.com>
2024-10-30 11:09:15 +0700

    add padding

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <>
       <ul className="flex flex-row bg-black">
-        <li><Link className="text-white text-xs tablet:text-xl desktop:text-5xl" href='/colors'>Color</Link></li>
+        <li>
+          <div className="p-4">
+          <Link className="text-white text-xs tablet:text-xl desktop:text-5xl" href='/colors'>Color</Link>
+          </div>
+        </li>
       </ul>
     </>
   );
